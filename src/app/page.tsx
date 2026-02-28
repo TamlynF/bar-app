f22c5f9692bc67affe357213cdd5298435307f95
TamlynF <tamlynf@chasesoftware.biz>
2026-02-28 01:57:57 +0000

    added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,51 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import BookingForm from "./(public)/_components/booking-form";
 
-export default function Home() {
+
+export default function HomePage() {
+  const [openAuthSheet, setOpenAuthSheet] = React.useState(false);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col">
+      <div className="bg-primary px-20 py-5 flex justify-between items-center">
+        <h1 className="text-xl font-bold text-white">S . H . E . Y</h1>
+        <Button variant={"outline"} onClick={() => setOpenAuthSheet(true)}>
+          Login
+        </Button>
+      </div>
+
+      <div className="grid lg:grid-cols-2 items-center h-[85vh] px-20">
+        <div className="col-span-1 mt-20 lg:mt-0">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-lg font-bold text-primary">
+              Discover & Book Your Dream Vacation
+            </h1>
+            <p className="text-sm text-gray-700 font-semibold">
+              Welcome to Shey Travel Packages — your go-to platform for
+              discovering unforgettable travel experiences. Browse exclusive
+              travel packages, book hassle-free getaways, and explore curated
+              destinations from around the world.
+            </p>
+
+            <Button className="w-max">Get started</Button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
+      {openAuthSheet && (
+        <Sheet open={openAuthSheet} onOpenChange={setOpenAuthSheet}>
+          <SheetContent className="lg:min-w-125">
+            <SheetHeader>
+              <SheetTitle></SheetTitle>
+            </SheetHeader>
+            <BookingForm />
+          </SheetContent>
+        </Sheet>
+      )}
     </div>
   );
 }
