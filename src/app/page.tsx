3b6edea49c87818427b33db1a3c734683ae87ee0
TamlynF <tamlynf@chasesoftware.biz>
2026-04-03 21:40:22 +0100

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  redirect("/book");
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Redirecting...</h1>
+      <Button variant="default" onClick={() => redirect("/login")} className="ml-4">
+        Login
+      </Button>
+      <Button variant="secondary" onClick={() => redirect("/bookings")} className="ml-4">
+        Bookings
+      </Button>
+    </div>
+  )
 }
