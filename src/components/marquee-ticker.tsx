53612d1b5d0768160e620dea790735418aa4c2a4
Tamlyn <tamlynf@chasesoftware.biz>
2026-07-23 05:31:47 +0100

    feat: update marquee animation duration to be configurable via CSS variable

--- src/components/marquee-ticker.tsx
+++ src/components/marquee-ticker.tsx
@@ -7,14 +7,21 @@ const PHRASES = [
   "Last Orders Late",
 ];
 
+const REPEATS_PER_HALF = 4;
+
 export function MarqueeTicker() {
   return (
     <div
       aria-hidden="true"
       className="relative -mx-[6%] w-[112%] -rotate-[1.5deg] overflow-hidden bg-[#FDCC4B] py-1.5 shadow-md shadow-black/25"
     >
-      <div className="ad-marquee-track">
-        {[0, 1].map((row) => (
+      <div
+        className="ad-marquee-track"
+        style={
+          { "--marquee-duration": `${REPEATS_PER_HALF * 30}s` } as React.CSSProperties
+        }
+      >
+        {Array.from({ length: REPEATS_PER_HALF * 2 }, (_, row) => (
           <div className="flex" key={row}>
             {PHRASES.map((phrase, i) => (
               <span
